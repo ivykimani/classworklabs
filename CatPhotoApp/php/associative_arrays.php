341e1356a6e7dad07cf519b9a02bd451d6f419e2
Ivy Kimani <ivy.kimani@strathmore.edu>
2026-09-24 15:48:30 +0300

    slight chage

--- CatPhotoApp/php/associative_arrays.php
+++ CatPhotoApp/php/associative_arrays.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <h1>Associative Arrays in PHP</h1>
+        <p>Associative arrays are arrays that use named keys that you assign to them. In this example, we have an associative array called <code>$grades</code>, where the keys are student names and the values are their corresponding grades. When a user submits a student's name through the form, the PHP code retrieves and displays the grade for that student using the associative array.</p>
 
         <form action="associative_arrays.php" method="post">
             <input type="text" name="student">
@@ -14,6 +15,7 @@
         </form>
 
         <?php
+
         $grades = array(
          "Figeuroth Faeth" => "B",
          "Riz Gukgak" => "A",
@@ -21,7 +23,18 @@
          "Fabian Seacaster" => "C+",
          "Gorgug ThistleSpring" => "B-",
          "Kristen Applebees" => "B+");
-          echo $grades[$_POST["student"]];
+          echo $grades[$_POST["student"]]; // This line retrieves the grade for the student name submitted through the form
+        
+
+        if (isset($_POST["student"])) {
+            $student = $_POST["student"];
+            if (array_key_exists($student, $grades)) {
+                echo "<p>The grade for $student is: " . $grades[$student] . "</p>";
+            } else {
+                echo "<p>Student not found.</p>";
+            }
+        }
+
         ?>
 
     </body>
